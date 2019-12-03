e037575d0d8075bac05786ffe10ad0e5462672c2
beathunter <nikitasherepa@gmail.com>
2019-12-03 21:28:56 +0300

    Lesson 5: сделал longestSimplePath(), в процессе выполнения largestIndependentVertexSet()

--- src/lesson5/JavaGraphTasks.java
+++ src/lesson5/JavaGraphTasks.java
@@ -2,8 +2,7 @@ package lesson5;
 
 import kotlin.NotImplementedError;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaGraphTasks {
@@ -96,7 +95,56 @@ public class JavaGraphTasks {
      * Эта задача может быть зачтена за пятый и шестой урок одновременно
      */
     public static Set<Graph.Vertex> largestIndependentVertexSet(Graph graph) {
-        throw new NotImplementedError();
+
+        Map<Graph.Vertex, Boolean> visitedVerticesMap = new HashMap<>();
+        for (Graph.Vertex vertex : graph.getVertices()) visitedVerticesMap.put(vertex, false);
+
+
+
+        Set<Graph.Vertex> independentVerticesSet = new HashSet<>();
+        Deque<Graph.Vertex> arrVertices = new LinkedList<>(graph.getVertices()); //K
+        Deque<Graph.Vertex> independentVertexList = new LinkedList<>();          //M
+        Deque<Graph.Vertex> visitedVerticesList = new LinkedList<>();            //P
+        Deque<Graph.Vertex> verticesList = new LinkedList<>();
+
+        //поиск циклов в графе
+//        if (!graph.getVertices().isEmpty()) loopSearch(graph, arrVertices.getFirst(), visitedVerticesMap);
+
+        //Алгоритм Брона-Кербоша
+        while(!arrVertices.isEmpty() || !independentVertexList.isEmpty()) {
+            Graph.Vertex vertex = null;
+            if (!arrVertices.isEmpty()) { //пока множество кандидатов не пустое
+                vertex = arrVertices.getFirst(); //берем вершину из множества кадидатов
+                independentVertexList.offer(vertex); //помещаем её в возможное независимое множество
+                for (Graph.Vertex neighbour : graph.getNeighbors(vertex)) arrVertices.remove(neighbour); //удаляем из множества кандидатов всех соседей вершины vertex
+                arrVertices.remove(vertex); //удаляем саму вершину vertex из множества кандидатов
+//                for (Graph.Vertex neighbour : graph.getNeighbors(vertex)) visitedVerticesList.remove(neighbour);
+            }
+            else {  //если множество кандидатов пустое
+//                if (visitedVerticesList.isEmpty()) {
+                independentVerticesSet.addAll(independentVertexList); //преобразуем List независимых вершин в Set
+                independentVertexList.clear();  //очищаем List для дальнейшего нового составления
+//                }
+//                arrVertices.remove(vertex);
+//                visitedVerticesList.offer(vertex);
+            }
+        }
+        return independentVerticesSet;
+    }
+
+    static void loopSearch(Graph graph, Graph.Vertex vertex, Map<Graph.Vertex, Boolean> visitedVerticesMap) {
+        boolean hasLoop = false;
+        List<Graph.Vertex> arrVertices = new ArrayList<>(graph.getVertices());
+
+        visitedVerticesMap.put(vertex, true);
+
+        Set<Graph.Vertex> visitedNeighbours = new HashSet<>();
+        for (Graph.Vertex neighbour : graph.getNeighbors(vertex)) {
+            if (visitedVerticesMap.get(neighbour) == true) visitedNeighbours.add(neighbour);
+            else loopSearch(graph, neighbour, visitedVerticesMap);
+        }
+        if (visitedNeighbours.equals(graph.getNeighbors(vertex)) && visitedNeighbours.size() > 1) hasLoop = true;
+        if (hasLoop) throw new IllegalArgumentException();
     }
 
     /**
@@ -120,6 +168,34 @@ public class JavaGraphTasks {
      * Ответ: A, E, J, K, D, C, H, G, B, F, I
      */
     public static Path longestSimplePath(Graph graph) {
-        throw new NotImplementedError();
+        Path longestPath = new Path();
+        int maxLength = -1;
+        Deque<Path> pathDeque = new LinkedList<>();
+        //составляем все возможные начала путей в графе
+        for (Graph.Vertex vertex : graph.getVertices()) pathDeque.offer(new Path(vertex));
+        while (!pathDeque.isEmpty()) {
+
+            //поочередно вытаскиваем пути, для дальнейшего их дополнения
+            Path path = pathDeque.poll();
+
+            for (Graph.Vertex neighbor : graph.getNeighbors(path.getVertices().get(path.getLength()))) {
+
+                //дополняем пути и помещаем их в pathDeque
+                if (!path.contains(neighbor)) pathDeque.offer(new Path(path, graph, neighbor));
+//                if (path.getVertices().contains(edge.getBegin())) break;
+//                if (edge.getBegin() != vertex) {
+//                    pathDeque.offer(edge.getBegin());
+//                }
+//                if (edge.getEnd() != vertex) {
+//                    pathDeque.offer(edge.getEnd());
+//                }
+            }
+
+            if (path.getLength() > maxLength) {
+                maxLength = path.getLength();
+                longestPath = path;
+            }
+        }
+        return longestPath;
     }
 }
